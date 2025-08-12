6016323ef4fd94d0e4b363d842b66558227dbdf0
Ashish Mhetre <amhetre@nvidia.com>
2025-08-12 06:32:27 +0000

    iommu/arm-smmu-v3: Implement WAR for Tegra264 Errata T264-SMMU-3
    If there is active traffic on a page while it is being invalidated, it
    is possible that the intended invalidated entry is not properly evicted
    from the SMMU TLB caches. Subsequent requests to the same virtual
    address could get the older mapping and access the wrong location, which
    can be a functional and a security problem.
    Repeat the entire invalidation->sync sequence once for every
    invalidation queued. The second set of invalidation + sync commands is
    guaranteed to evict the entry out of all TLBs.
    
    Bug 5448714
    Bug 5150304
    
    Change-Id: I40e891a1459f4c65e4e2e7f0224867d6c1be277a
    (cherry-picked from commit 27c7fb19c438457aabe7d7b4812bf88778750017)
    Signed-off-by: Ashish Mhetre <amhetre@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/canonical/linux-noble/+/3496080
    GVS: buildbot_gerritrpt <buildbot_gerritrpt@nvidia.com>
    Reviewed-by: Pritesh Raithatha <praithatha@nvidia.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -83,6 +83,7 @@ DEFINE_MUTEX(arm_smmu_asid_lock);
 static struct arm_smmu_option_prop arm_smmu_options[] = {
 	{ ARM_SMMU_OPT_SKIP_PREFETCH, "hisilicon,broken-prefetch-cmd" },
 	{ ARM_SMMU_OPT_PAGE0_REGS_ONLY, "cavium,cn9900-broken-page1-regspace"},
+	{ ARM_SMMU_OPT_TLBI_TWICE, "nvidia,tegra264-tlbi-race-fix"},
 	{ 0, NULL},
 };
 
@@ -1241,6 +1242,8 @@ static void arm_smmu_sync_cd(struct arm_smmu_master *master,
 	}
 
 	arm_smmu_cmdq_batch_submit(smmu, &cmds);
+	if (smmu->options & ARM_SMMU_OPT_TLBI_TWICE)
+		arm_smmu_cmdq_batch_submit(smmu, &cmds);
 }
 
 static void arm_smmu_write_cd_l1_desc(struct arm_smmu_cdtab_l1 *dst,
@@ -2359,6 +2362,8 @@ static void arm_smmu_tlb_inv_range_domain(unsigned long iova, size_t size,
 		cmd.tlbi.vmid	= smmu_domain->s2_cfg.vmid;
 	}
 	__arm_smmu_tlb_inv_range(&cmd, iova, size, granule, smmu_domain);
+	if (smmu_domain->smmu->options & ARM_SMMU_OPT_TLBI_TWICE)
+		__arm_smmu_tlb_inv_range(&cmd, iova, size, granule, smmu_domain);
 
 	if (smmu_domain->nest_parent) {
 		/*
@@ -2390,6 +2395,8 @@ void arm_smmu_tlb_inv_range_asid(unsigned long iova, size_t size, int asid,
 	};
 
 	__arm_smmu_tlb_inv_range(&cmd, iova, size, granule, smmu_domain);
+	if (smmu_domain->smmu->options & ARM_SMMU_OPT_TLBI_TWICE)
+		__arm_smmu_tlb_inv_range(&cmd, iova, size, granule, smmu_domain);
 }
 
 static void arm_smmu_tlb_inv_page_nosync(struct iommu_iotlb_gather *gather,
@@ -4169,15 +4176,21 @@ static int arm_smmu_device_reset(struct arm_smmu_device *smmu)
 	/* Invalidate any cached configuration */
 	cmd.opcode = CMDQ_OP_CFGI_ALL;
 	arm_smmu_cmdq_issue_cmd_with_sync(smmu, &cmd);
+	if (smmu->options & ARM_SMMU_OPT_TLBI_TWICE)
+		arm_smmu_cmdq_issue_cmd_with_sync(smmu, &cmd);
 
 	/* Invalidate any stale TLB entries */
 	if (smmu->features & ARM_SMMU_FEAT_HYP) {
 		cmd.opcode = CMDQ_OP_TLBI_EL2_ALL;
 		arm_smmu_cmdq_issue_cmd_with_sync(smmu, &cmd);
+		if (smmu->options & ARM_SMMU_OPT_TLBI_TWICE)
+			arm_smmu_cmdq_issue_cmd_with_sync(smmu, &cmd);
 	}
 
 	cmd.opcode = CMDQ_OP_TLBI_NSNH_ALL;
 	arm_smmu_cmdq_issue_cmd_with_sync(smmu, &cmd);
+	if (smmu->options & ARM_SMMU_OPT_TLBI_TWICE)
+		arm_smmu_cmdq_issue_cmd_with_sync(smmu, &cmd);
 
 	/* Event queue */
 	writeq_relaxed(smmu->evtq.q.q_base, smmu->base + ARM_SMMU_EVTQ_BASE);
